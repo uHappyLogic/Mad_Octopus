f04e398a556a190701fcc86ae6bd50de8a1fa60e
uHappyLogic <kosiak.lukasz@gmail.com>
2017-06-06 01:39:36 +0200

    Event space limited

--- 117272_117269.py
+++ 117272_117269.py
@@ -1,14 +1,18 @@
 import random
 from array import array
 
-class Agent:
-    "A template agent acting randomly"
 
-    # name should contain only letters, digits, and underscores (not enforced by environment)
+class Agent:
+
     __name = '117272_117269'
 
     def __init__(self, stateDim, actionDim, agentParams):
-        "Initialize agent assuming floating point state and action"
+
+        print("===========================================")
+        print('params {0}'.format(agentParams))
+        print('state dim {0} actionDim {1}'.format(stateDim, actionDim))
+        print("===========================================")
+
         self.__stateDim = stateDim
         self.__actionDim = actionDim
         self.__action = array('d', [0 for x in range(actionDim)])
@@ -27,7 +31,7 @@ class Agent:
 
     def __curlAction(self):
         for i in range(self.__actionDim):
-            if (i % 3 == 2):
+            if i % 3 == 2:
                 self.__action[i] = 1
             else:
                 self.__action[i] = 0
@@ -37,7 +41,7 @@ class Agent:
         "Given starting state, agent returns first action"
         self.__randomAction()
 
-        self.last_step_log_line = [self.__action] #, state]
+        self.log_state(state)
 
         return self.__action
 
@@ -45,18 +49,55 @@ class Agent:
         print('step {0}'.format(self.step_id))
         self.step_id += 1
 
-        self.last_step_log_line.append(reward)
-
-        self.run_log_file.write(','.join(map(str, self.last_step_log_line)) + '\n')
+        self.log_reward(reward)
 
         "Given current reward and state, agent returns next action"
-        self.__curlAction()
+        self.__randomAction()
 
-        self.last_step_log_line = [self.__action, state]
+        self.log_state(state)
 
         return self.__action
 
+    def log_state(self, state):
+        l_state = list(state)
+
+        pos = []
+        vel = []
+
+        # position
+        pos.append((l_state[2] + l_state[42]) / 2)
+        pos.append((l_state[3] + l_state[43]) / 2)
+
+        for i in range(36):
+            if i % 8 == 0:
+                pos.append((l_state[i + 6] + l_state[i + 46]) / 2)
+                pos.append((l_state[i + 7] + l_state[i + 47]) / 2)
+
+        vel.append((l_state[4] + l_state[44]) / 2)
+        vel.append((l_state[5] + l_state[45]) / 2)
+
+        for i in range(36):
+            if i % 8 == 0:
+                vel.append((l_state[i + 8] + l_state[i + 48]) / 2)
+                vel.append((l_state[i + 9] + l_state[i + 49]) / 2)
+
+        state_rep = ','.join(['{: 3.2f}'.format(el) for el in pos]) + '\n'
+        state_rep += ','.join(['{: 3.2f}'.format(el) for el in vel]) + '\n'
+
+        # self.last_step_log_line = ["%.2f" % round(el, 2) for el in self.__action]
+        # self.last_step_log_line = ["%.2f" % el for el in state]
+
+        self.last_step_log_line = [state_rep]
+
+    def log_reward(self, reward):
+        self.last_step_log_line.append(reward)
+
+        self.run_log_file.write(','.join(map(str, self.last_step_log_line)) + ('\n--{0}-\n'.format(self.step_id)))
+
     def end(self, reward):
+
+        self.log_reward(reward)
+
         self.run_log_file.close()
 
     def cleanup(self):
@@ -64,5 +105,3 @@ class Agent:
 
     def getName(self):
         return self.__name
-
-
