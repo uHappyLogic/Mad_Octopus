b9570c10105d22131fc4f866324e6612c0a2cf8e
uHappyLogic <kosiak.lukasz@gmail.com>
2017-06-10 00:36:34 +0200

    working

--- misio_agent/117272_117269.py
+++ misio_agent/117272_117269.py
@@ -1,10 +1,10 @@
 from array import array
 import numpy as np
 import math
+import json
+import os
 
 from pytools import flatten
-
-from Logger import Logger
 from Model import Model
 
 
@@ -12,12 +12,6 @@ class Agent:
     __name = '117272_117269'
 
     def __init__(self, stateDim, actionDim, agentParams):
-
-        print("===========================================")
-        print('params {0}'.format(agentParams))
-        print('state dim {0} actionDim {1}'.format(stateDim, actionDim))
-        print("===========================================")
-        # self.__logger = Logger()
         self.__terget_point = [9, -1]
         self.__stateDim = stateDim
         self.__actionDim = actionDim
@@ -39,6 +33,13 @@ class Agent:
 
         self.model = Model(0.5, self.neural_action_count, 42, 10)
 
+        self.model_dump_file_name = "model.h5"
+
+        if os.path.exists("model.h5"):
+            self.model.model.load_weights(self.model_dump_file_name)
+        else:
+            print("Model cannot be loaded")
+
         self.previous_state = []
 
     def __randomAction(self):
@@ -46,8 +47,6 @@ class Agent:
         bin_action = bin(self.action_idx)
         self.__action_set = [int(i) for i in list(bin_action)[2:]]
         self.__action = self.unwind_action(self.__action_set)
-        #for i in range(self.__actionDim):
-        #    self.__action[i] = random.random()
 
     def __neural_action(self, state):
         neural_output = self.model.model.predict(np.array([state]))[0]
@@ -58,10 +57,9 @@ class Agent:
         bests_neural_outputs = sorted_indexed_neural_output[0:bests_count]
         best_probabilites = [x[1] for x in bests_neural_outputs]
         prob_sum = np.sum(best_probabilites)
-        print("prob sum {0}".format(prob_sum))
         normalized_best_probs = [i/prob_sum for i in best_probabilites]
         print("norm best probs {0}".format(normalized_best_probs))
-        print("best neural output {0}".format(bests_neural_outputs))
+        print("best neural output {0}".format([ bests_neural_outputs]))
 
         current_prob = 0
         treshold = np.random.random()
@@ -72,31 +70,30 @@ class Agent:
                 best_id = i
                 break
 
-        print(neural_output)
+        print("choosen action {0}".format(bests_neural_outputs[best_id][0]))
+
         self.action_idx = bests_neural_outputs[best_id][0]
-        print('argmax = {0}'.format(self.action_idx))
         bin_action = bin(self.action_idx)
         self.__action_set = [int(i) for i in list(bin_action)[2:]]
         self.__action = self.unwind_action(self.__action_set)
 
-    def __curlAction(self):
-        for i in range(self.__actionDim):
-            if self.counter % 100 < 50:
-                if i % 3 == 0:
-                    self.__action[i] = 1
-                if i % 3 == 2:
-                    self.__action[i] = 0
-                if i % 3 == 1:
-                    self.__action[i] = 0
-            else:
-                if i % 3 == 0:
-                    self.__action[i] = 0
-                if i % 3 == 2:
-                    self.__action[i] = 1
-                if i % 3 == 1:
-                    self.__action[i] = 0
+    # def __curlAction(self):
+    #     for i in range(self.__actionDim):
+    #         if self.counter % 100 < 50:
+    #             if i % 3 == 0:
+    #                 self.__action[i] = 1
+    #             if i % 3 == 2:
+    #                 self.__action[i] = 0
+    #             if i % 3 == 1:
+    #                 self.__action[i] = 0
+    #         else:
+    #             if i % 3 == 0:
+    #                 self.__action[i] = 0
+    #             if i % 3 == 2:
+    #                 self.__action[i] = 1
+    #             if i % 3 == 1:
+    #                 self.__action[i] = 0
 
-    # return minimal between snake and target
     def get_distance_from_target(self, simple_state):
         dists = []
         for i in range(len(simple_state)):
@@ -115,14 +112,9 @@ class Agent:
         return result
 
     def start(self, state):
-        print('agent started')
-        "Given starting state, agent returns first action"
-        self.__randomAction()
-        # self.__logger.log_state(state)
-
         self.previous_state = self.get_flat_simple_state(list(state))
 
-        print(self.previous_state)
+        self.__neural_action(self.previous_state)
 
         return self.__action
 
@@ -131,25 +123,16 @@ class Agent:
         state_list = list(state)
         if self.counter % self.batch_size == 1:
             inputs, targets = self.model.exp_replay.get_batch(self.model.model, self.batch_size)
-            print("inputs")
-            print(inputs)
-            print("targets")
-            print(targets)
-            loss = self.model.model.train_on_batch(inputs, targets)
-            print("loss: {0}".format(loss))
+            self.model.model.train_on_batch(inputs, targets)
 
         enhanced_reward = pow(10 - (self.get_distance_from_target(self.get_simple_state(state_list))), 2) / 100
 
         self.step_id += 1
-        # self.__logger.log_reward(enhanced_reward, self.step_id)
-        print("enhanced reward {0}".format(enhanced_reward))
-
         current_state = self.get_flat_simple_state(state_list)
-        self.model.remember(self.previous_state, self.action_idx, enhanced_reward, current_state, False)
+
+        self.model.remember(self.previous_state, self.action_idx, enhanced_reward, current_state)
 
         self.previous_state = current_state
-        #self.__curlAction()
-        #self.__randomAction()
         self.__neural_action(current_state)
         self.counter += 1
 
@@ -161,7 +144,7 @@ class Agent:
     def get_simple_state(self, state):
         simple_state = []
         l_state = state
-        # average points
+
         for i in range(2, 42, 4):
             x = (l_state[i] + l_state[i + 40]) / 2
             y = (l_state[i + 1] + l_state[i + 41]) / 2
@@ -169,20 +152,22 @@ class Agent:
             v_y = (l_state[i + 3] + l_state[i + 43]) / 2
             state_part = [x, y, v_x, v_y]
             simple_state.append(state_part)
-        # start_angle_and_velocity
+
         simple_state.append([l_state[0], l_state[1]])
         return simple_state
 
     def end(self, reward):
         pass
-        # self.__logger.log_reward(reward, self.step_id)
-        # self.__logger.close()
 
     def cleanup(self):
-        pass
+        inputs, targets = self.model.exp_replay.get_batch(self.model.model, self.batch_size)
+        self.model.model.train_on_batch(inputs, targets)
+
+        print("saving dump")
+        self.model.model.save_weights(self.model_dump_file_name, overwrite=True)
+
+        print("end")
 
     def getName(self):
         return self.__name
 
-    def __str__(self) -> str:
-        return super().__str__()
--- misio_agent/Model.py
+++ misio_agent/Model.py
@@ -1,7 +1,6 @@
-import json
 import numpy as np
-from keras.models import Sequential
 from keras.layers.core import Dense
+from keras.models import Sequential
 from keras.optimizers import sgd
 
 
@@ -13,33 +12,28 @@ class ExperienceReplay(object):
         self.action_count = action_count
         self.state_count = state_count
 
-    def remember(self, states, game_over):
-        # memory[i] = [[state_t, action_t, reward_t, state_t+1], game_over?]
-        self.memory.append([states, game_over])
+    def remember(self, states):
+        self.memory.append([states])
         if len(self.memory) > self.max_memory:
             del self.memory[0]
 
-    def get_batch(self, model, batch_size=10):
+    def get_batch(self, model, batch_size):
         len_memory = len(self.memory)
         env_dim = self.state_count
-        inputs = np.zeros((min(len_memory, batch_size), env_dim))
-        targets = np.zeros((inputs.shape[0], self.action_count))
-        print(targets)
-        for i, idx in enumerate(np.random.randint(0, len_memory, size=[2])):
-            print('iterator: {0}'.format(i))
-            print('idx: {0}'.format(idx))
-            state_t, action_t, reward_t, state_tp1 = self.memory[idx][0]
-            print("action_t: {0}".format(action_t))
-            game_over = self.memory[idx][1]
 
+        poll_size = min(len_memory, batch_size)
+
+        inputs = np.zeros((poll_size, env_dim))
+        targets = np.zeros((inputs.shape[0], self.action_count))
+
+        for i, idx in enumerate(np.random.randint(0, len_memory, size=poll_size)):
+            state_t, action_t, reward_t, state_tp1 = self.memory[idx][0]
             inputs[i:i+1] = state_t
             targets[i] = model.predict(state_t)[0]
+
             Q_sa = np.max(model.predict(state_tp1)[0])
-            if game_over:  # if game_over is True
-                targets[i, action_t] = reward_t
-            else:
-                # reward_t + gamma * max_a' Q(s', a')
-                targets[i, action_t] = reward_t + self.discount * Q_sa
+
+            targets[i, action_t] = reward_t + self.discount * Q_sa
         return inputs, targets
 
 
@@ -56,10 +50,10 @@ class Model:
         self.model.add(Dense(self.hidden_nodes_number, input_shape=(self.states_count, ), activation='relu'))
         self.model.add(Dense(self.hidden_nodes_number, activation='relu'))
         self.model.add(Dense(self.actions_count, activation='sigmoid'))
-        self.model.compile(sgd(lr=.2), "mse")
-        max_memory = 10
+        self.model.compile(sgd(lr=.3), "mse")
+        max_memory = 100
 
         self.exp_replay = ExperienceReplay(self.actions_count, self.states_count, max_memory=max_memory, discount=0.9)
 
-    def remember(self, state_before_move, action, reward, state_after_move, game_over):
-        self.exp_replay.remember([np.array([state_before_move]), action, reward, np.array([state_after_move])], game_over)
+    def remember(self, state_before_move, action, reward, state_after_move):
+        self.exp_replay.remember([np.array([state_before_move]), action, reward, np.array([state_after_move])])
